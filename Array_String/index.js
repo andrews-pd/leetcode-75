3ddd69428a7c026766b7b767db635d61bd60f6f8
Andrews Santiago <83369097+andrews-pd@users.noreply.github.com>
2025-04-16 15:22:30 -0300

    feat adding array-string chanllanges

---
+++ Array_String/index.js
@@ -0,0 +1,4 @@
+const gcdOfStrings = require('./gcdOfStrings');
+const mergeAlternately = require('./mergeAlternately');
+
+module.exports = { gcdOfStrings, mergeAlternately };
---
+++ index.js
@@ -0,0 +1,4 @@
+const { gcdOfStrings, mergeAlternately } = require("./Array_String");
+
+console.log("Resultado gcdOfStrings: ", gcdOfStrings("ABCABC", "ABC"));
+console.log("Resultado mergeAlternately: ", mergeAlternately("abc", "pqr"));
