e6a6d204e59a8f274376f6ee8c9dc849153fc7da
Andrews Santiago <83369097+andrews-pd@users.noreply.github.com>
2025-04-23 21:58:05 -0300

    feat: adding first missing positive challange

--- HashMap_Set/index.js
+++ HashMap_Set/index.js
@@ -1,11 +1,13 @@
 const closeStrings = require("./closeStrings");
 const equalPairs = require("./equalPairs");
 const findDifference = require("./findDifference");
+const firstMissingPositive = require("./firstMissingPositive");
 const uniqueOccurrences = require("./uniqueOccurrences");
 
 module.exports = { 
   findDifference,
   uniqueOccurrences,
   closeStrings,
-  equalPairs
+  equalPairs,
+  firstMissingPositive
 };
